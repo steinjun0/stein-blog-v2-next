f6003ba6abcf8d717d63123731eb42c074126de3
steinjun0 <1142308@naver.com>
2022-10-31 02:18:06 +0000

    홈화면 컴포넌트 추가 - 이미지 교체 - 포스트 진입 컴포넌트 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/image';
 import HomeTitle from '../components/HomeTitle';
 import styles from '../styles/Home.module.css'
+
+
+function PostComponent({ categories, title, subtitle }: { categories: string[], title: string, subtitle: string }) {
+  return (
+    <div className="w-64 h-40 flex-col justify-center border-b-slate-400 border-b">
+      <span className='text-sm'>[{categories.toString().replaceAll(',', ', ')}]</span>
+      <h1 className='text-2xl font-medium h-16 mt-1 whitespace-pre-wrap'>{title}</h1>
+      <p className='mt-3 mb-5 whitespace-pre-wrap'>{subtitle}</p>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -11,10 +22,19 @@ export default function Home() {
           <HomeTitle />
         </div>
         <div className='flex-col ml-20'>
-          <div className='w-10 bg-slate-500' style={{ width: '480px', height: '480px' }}>image</div>
-          <div>post</div>
+          <Image
+            src={'/images/profile.png'}
+            alt='profile'
+            width={480}
+            height={480} />
         </div>
       </div>
+      <section className="flex justify-center items-center w-100 h-52 border-slate-500 border">
+
+        <PostComponent categories={['cat1', 'cat2']} title={'이 글의 제목은 \n"무엇이 제목인가"'} subtitle={'제목과 부제목에 대한 간단한 고찰'} />
+        <PostComponent categories={['cat1', 'cat2']} title={'여러가지 상황'} subtitle={'부제목의 길고 긴 길이.\n그것은 두줄이 되기에 충분했다'} />
+      </section>
+
     </div>
   );
 }
