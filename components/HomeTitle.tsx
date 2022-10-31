733ba44f6547ad32072dbc8c6265f2e106888d2a
steinjun0 <1142308@naver.com>
2022-10-31 07:52:59 +0000

    반응형으로 수정

--- components/HomeTitle.tsx
+++ components/HomeTitle.tsx
@@ -6,22 +6,22 @@ export default function HomeTitle() {
   const [hoverIndex, setHoverIndex] = useState<number>(-1)
 
   return (
-    <div className='flex-col' >
+    <div className='flex-col overflow-y-clip' >
       <div
         onClick={() => setSelectIndex(0)}
         onMouseEnter={() => setHoverIndex(0)}
         onMouseLeave={() => setHoverIndex(-1)}
         className="flex-col justify-end"
-        style={{ cursor: 'pointer' }}
+        style={{ cursor: 'pointer', overflowY: 'clip' }}
       >
 
         {selectIndex === 0 ?
-          <p className="font-bold" style={{ fontSize: `${selectIndex === 0 ? '220px' : '200px'}`, lineHeight: '200px' }}>Post</p>
+          <p className="font-bold transition-all" style={{ fontSize: `${selectIndex === 0 ? '13.8vw' : '15.2vw'}`, lineHeight: '13.8vw' }}>Post</p>
           :
           <p
             onClick={() => setSelectIndex(0)}
             className="font-bold transition-all"
-            style={{ lineHeight: '180px', WebkitTextStroke: 'calc(0.005em + 1px) #000', color: 'transparent', fontSize: `${hoverIndex === 0 ? '220px' : '200px'}` }}>
+            style={{ lineHeight: '180px', WebkitTextStroke: 'calc(0.005em + 1px) #000', color: 'transparent', fontSize: `${hoverIndex === 0 ? '13.8vw' : '15.2vw'}` }}>
             Post
           </p>
         }
@@ -35,12 +35,12 @@ export default function HomeTitle() {
         style={{ cursor: 'pointer', overflowY: 'clip' }}
       >
         {selectIndex === 1 ?
-          <p className="font-bold" style={{ fontSize: `${selectIndex === 1 ? '220px' : '200px'}`, lineHeight: '200px' }}>Work</p>
+          <p className="font-bold transition-all" style={{ fontSize: `${selectIndex === 1 ? '13.8vw' : '15.2vw'}`, lineHeight: '13.8vw' }}>Work</p>
           :
           <p
             onClick={() => setSelectIndex(1)}
             className="font-bold transition-all"
-            style={{ lineHeight: '200px', WebkitTextStroke: 'calc(0.005em + 1px) #000', color: 'transparent', fontSize: `${hoverIndex === 1 ? '220px' : '200px'}` }}>
+            style={{ lineHeight: '180px', WebkitTextStroke: 'calc(0.005em + 1px) #000', color: 'transparent', fontSize: `${hoverIndex === 1 ? '13.8vw' : '15.2vw'}` }}>
             Work
           </p>
         }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,87 @@
 import Head from 'next/head'
 import Image from 'next/image';
+import { Suspense, useEffect, useState } from 'react';
 import HomeTitle from '../components/HomeTitle';
+import useWindowSize from '../components/hooks/useWindowSize';
 import styles from '../styles/Home.module.css'
 
+interface IPostComponent { categories: string[], title: string, subtitle: string }
 
-function PostComponent({ categories, title, subtitle }: { categories: string[], title: string, subtitle: string }) {
+function PostComponent({ categories, title, subtitle }: IPostComponent) {
   return (
-    <div className="w-64 h-40 flex-col justify-center border-b-slate-400 border-b">
+    <div className="w-64 h-44 flex-col justify-center border-b-slate-400 border-b">
       <span className='text-sm'>[{categories.toString().replaceAll(',', ', ')}]</span>
       <h1 className='text-2xl font-medium h-16 mt-1 whitespace-pre-wrap'>{title}</h1>
-      <p className='mt-3 mb-5 whitespace-pre-wrap'>{subtitle}</p>
+      <p className='mt-3 whitespace-pre-wrap'>{subtitle}</p>
     </div>
   );
 }
 
-export default function Home() {
-  return (
-    <div className='container'>
-      <div className='flex justify-center items-center  w-full'>
-        <div className='flex-col w-1/2'>
-          <HomeTitle />
-        </div>
-        <div className='flex-col ml-20'>
-          <Image
-            src={'/images/profile.png'}
-            alt='profile'
-            width={480}
-            height={480} />
-        </div>
-      </div>
-      <section className="flex justify-center items-center w-100 h-52 border-slate-500 border">
+function getBreakPoint(width: number): string {
+  if (width > 1536) {
+    return '2xl'
+  } else if (width > 1280) {
+    return 'xl'
+  } else if (width > 1024) {
+    return 'lg'
+  } else if (width > 768) {
+    return 'md'
+  } else if (width > 640) {
+    return 'sm'
+  } else {
+    return 'None'
+  }
+}
 
-        <PostComponent categories={['cat1', 'cat2']} title={'이 글의 제목은 \n"무엇이 제목인가"'} subtitle={'제목과 부제목에 대한 간단한 고찰'} />
-        <PostComponent categories={['cat1', 'cat2']} title={'여러가지 상황'} subtitle={'부제목의 길고 긴 길이.\n그것은 두줄이 되기에 충분했다'} />
-      </section>
+function getMaxPostCount(breakPoint: string): number {
+  const maxPostCount: { [key: string]: number } = {
+    '2xl': 4,
+    'xl': 4,
+    'lg': 3,
+    'md': 2,
+    'sm': 2,
+    'None': 1
+  }
+  return maxPostCount[breakPoint]
+}
+
+export default function Home() {
+  const [posts, setPosts] = useState<IPostComponent[]>([])
+  const size = useWindowSize();
+  useEffect(() => {
+    const dummyPosts = [
+      { categories: ['cat1', 'cat2'], title: '이 글의 제목은 \n"무엇이 제목인가"', subtitle: '제목과 부제목에 대한 간단한 고찰' },
+      { categories: ['cat1', 'cat2'], title: '여러가지 상황\n미필적 고의도 범죄다', subtitle: '부제목의 길고 긴 길이.\n그것은 두줄이 되기에 충분했다' },
+      { categories: ['cat1', 'cat2'], title: '이 글의 제목은 \n"무엇이 제목인가"', subtitle: '제목과 부제목에 대한 간단한 고찰\n2줄로 나눠봤습니다' },
+      { categories: ['cat1', 'cat2'], title: '여러가지 상황', subtitle: '부제목의 길고 긴 길이.\n그것은 두줄이 되기에 충분했다' },
+    ]
+    setPosts(dummyPosts)
+  }, [])
+
+  return (
+    <div className='container flex flex-col justify-center' style={{ height: 'calc(100vh - 68px)' }}>
+      <div className='flex-col'>
+        <div className='flex justify-center items-center w-full'>
+          <div className='flex-col w-1/2'>
+            <HomeTitle />
+          </div>
+          <div className='flex-col justify-center ml-auto w-1/2 h-full'>
+            <div className='relative' style={{ width: '30vw', height: '30vw', left: 'calc(50% - 15vw)' }}>
+              <Image
+                src={'/images/profile.png'}
+                alt='profile'
+                fill
+                sizes='100vw'
+              />
+            </div>
+          </div>
+        </div>
+        <section className="flex justify-between px-8 mt-16 items-center w-100 h-52 border-slate-0 border-0">
+          {posts!.slice(0, getMaxPostCount(getBreakPoint(size.width))).map((e, i) =>
+            <PostComponent key={i} categories={['cat1', 'cat2']} title={e.title} subtitle={e.subtitle} />
+          )}
+        </section>
+      </div>
 
     </div>
   );
