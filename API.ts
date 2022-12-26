8c27570f5496ad7cc4bcfc084826354326045392
steinjun0 <1142308@naver.com>
2022-12-26 04:00:52 +0000

    API.js -> API.ts 변경 - javascript -> typescript 변경 - axios response string -> date객체로 자동 변환 기능 추가

---
+++ API.ts
@@ -0,0 +1,163 @@
+import axios from 'axios'
+import { AxiosError } from 'axios'
+
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.blog.steinjun.net' : '//localhost:8888'
+
+// let accessToken = localStorage.getItem('AccessToken')
+let accessToken = null
+let tokenHeader: { Authorization: string } | null = null
+
+
+if (accessToken !== null) {
+    tokenHeader = { Authorization: `${accessToken}` }
+} else {
+    tokenHeader = null
+}
+
+const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?/;
+
+function isIsoDateString(value: any): boolean {
+    return value && typeof value === "string" && isoDateFormat.test(value);
+}
+
+export function handleDates(body: string) {
+    const jsonBody: { string: any } = JSON.parse(body)
+
+    function recursiveChanger(json: { string: any }) {
+        for (const key of Object.keys(json)) {
+            const value = json[key as keyof { string: any }];
+            if (typeof value === 'object')
+                recursiveChanger(value)
+            else if (isIsoDateString(value)) {
+                json[key as keyof { string: any }] = new Date(value);
+            }
+        }
+    }
+
+    recursiveChanger(jsonBody)
+    return jsonBody
+}
+
+
+
+
+export default {
+    // api default
+
+    refreshUserData() {
+        // let accessToken = localStorage.getItem('AccessToken')
+        let accessToken = null
+
+        if (accessToken !== null) {
+            tokenHeader = { Authorization: `${accessToken}` }
+        } else {
+            tokenHeader = null
+        }
+    },
+    async getAxios(url: string) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.get(url, { transformResponse: handleDates, headers: tokenHeader })
+            : await axios.get(url, { transformResponse: handleDates })
+        return res
+    },
+    async getAxiosZip(url: string) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.get(url, { headers: { Authorization: tokenHeader.Authorization, accept: 'application/x-zip-compressed' } })
+            : await axios.get(url, { headers: { accept: 'application/x-zip-compressed' }, transformResponse: handleDates })
+        return res
+    },
+    async getAxiosWithParams(url: string, param: object) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.get(url, {
+            transformResponse: handleDates,
+            headers: tokenHeader,
+            params: param,
+        }) : await axios.get(url, {
+            transformResponse: handleDates,
+            params: param,
+        })
+        return res
+    },
+    async postAxios(url: string, data: object) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.post(url, data, { transformResponse: handleDates, headers: tokenHeader })
+            : await axios.post(url, data, { transformResponse: handleDates })
+        return res
+    },
+    async postAxiosFormData(url: string, data: object) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.post(url, data, { headers: { Authorization: tokenHeader.Authorization, 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
+            : await axios.post(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
+        return res
+    },
+    async patchAxios(url: string, data: object) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.patch(url, data, { headers: tokenHeader, transformResponse: handleDates }) : await axios.patch(url, data, { transformResponse: handleDates })
+        return res
+    },
+    async patchAxiosFormData(url: string, data: object) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.patch(url, data, { headers: { Authorization: tokenHeader.Authorization, 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
+            : await axios.patch(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
+        return res
+    },
+
+    async putAxios(url: string) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.put(url, { headers: tokenHeader, transformResponse: handleDates })
+            : await axios.put(url, { transformResponse: handleDates })
+        return res
+    },
+
+    async deleteAxios(url: string) {
+        this.refreshUserData()
+        const res = tokenHeader ? await axios.delete(url, { headers: tokenHeader, transformResponse: handleDates })
+            : await axios.delete(url, { transformResponse: handleDates })
+        return res
+    },
+
+    // api
+
+    async getPostList() {
+        const postListRes = await this.getAxios(`${API_URL}/post`)
+        return postListRes
+    },
+
+    async getPost({ id }: { id: number }) {
+        const postRes = await this.getAxios(`${API_URL}/post/${id}`)
+        return postRes
+    },
+
+    // async getConsultSchedule(year, month, mentorId) {
+    //     const scheduleRes = await this.getAxiosWithParams(`${API_URL}/consult/schedule`, { 'Year': year, 'Month': month, 'MentorID': +mentorId })
+    //     return scheduleRes
+    // },
+
+    // async postAccount(email, password, nickname) {
+    //     const res = await this.postAxios(`${API_URL}/account`, { email, password, nickname })
+    //     return res
+    // },
+
+    // async postAccountMentorFile(id, file) {
+    //     const scheduleRes = await this.postAxiosFormData(`${API_URL}/account/mentor/${id}/file`, file)
+    //     return scheduleRes
+    // },
+
+    // async patchAccount(userData) {
+    //     const userRes = await this.patchAxios(`${API_URL}/account/update`, userData)
+    //     return userRes
+    // },
+
+
+    // async patchConsultApprove(consultId) {
+    //     const consultRes = await this.patchAxios(`${API_URL}/consult/${consultId}/approve`)
+    //     return consultRes
+    // },
+
+    // async deleteConsultSchedule(shceduleId) {
+    //     const deleteRes = await this.deleteAxios(`${API_URL}/consult/schedule/${shceduleId}`)
+    //     return deleteRes
+    // },
+
+
+}
