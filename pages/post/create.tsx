5b9207838e7864cc36246a859a04662347fe80ea
steinjun0 <1142308@naver.com>
2023-01-01 02:42:41 +0000

    썸네일 업로드 기능 추가

--- pages/post/create.tsx
+++ pages/post/create.tsx
@@ -1,8 +1,8 @@
-import { Button, TextField } from "@mui/material";
+import { Button, Input, TextField } from "@mui/material";
 import API from "API";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 
 const MDEditor = dynamic(
@@ -58,6 +58,7 @@ export default function WorkPage() {
     const [categories, setCategories] = useState<Array<number>>([]);
     const [fileNames, setFileNames] = useState<Array<string>>([]);
     const [md, setMd] = useState<string>('') // for save
+    const [urlCacheBreaker, setUrlCacheBreaker] = useState<string>('')
     const mdRef = useRef<string>('') // for read
     const fileNamesRef = useRef<Array<string>>([])
 
@@ -80,57 +81,93 @@ export default function WorkPage() {
 
 
     useEffect(() => {
-        var x = new MutationObserver(function (e) {
-            const target = document.getElementsByClassName('w-md-editor-text-input')[0] as HTMLElement;
+        if (router.isReady) {
+            var x = new MutationObserver(function (e) {
+                const target = document.getElementsByClassName('w-md-editor-text-input')[0] as HTMLElement;
 
-            if (target !== undefined && !isSetListener) {
-                isSetListener = true
-                target.addEventListener('paste', async (event: any) => {
-                    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-                    console.log(JSON.stringify(items)); // might give you mime types
-                    for (var index in items) {
-                        var item = items[index];
-                        if (item.kind === 'file') {
-                            let file = item.getAsFile();
-                            let fileName: string = getUnduplicatedName(file.name, fileNamesRef.current)
+                if (target !== undefined && !isSetListener) {
+                    isSetListener = true
+                    target.addEventListener('paste', async (event: any) => {
+                        var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+                        for (var index in items) {
+                            var item = items[index];
+                            if (item.kind === 'file') {
+                                let file = item.getAsFile();
+                                let fileName: string = getUnduplicatedName(file.name, fileNamesRef.current)
 
-                            const res = await API.postFile({ file: file, name: fileName })
-                            if (res.status === 201) {
-                                setFileNames(fileNames => [...fileNames, fileName])
-                                fileNamesRef.current = [...fileNamesRef.current, fileName]
+                                const res = await API.postFile({ file: file, name: fileName })
+                                if (res.status === 201) {
+                                    setFileNames(fileNames => [...fileNames, fileName])
+                                    fileNamesRef.current = [...fileNamesRef.current, fileName]
 
-                                const newMd: string = mdRef.current + `<p align="center"><img src="${API.getPostFileUrl({ postId: 'temp', fileName: fileName })}" alt="${fileName}" style="max-height: 300px"/></p>`
-                                setMd(newMd)
-                            } else {
-                                alert('이미지가 업로드 되지 못하였습니다!')
+                                    const newMd: string = mdRef.current + `<p align="center"><img src="${API.getPostFileUrl({ postId: 'temp', fileName: fileName })}" alt="${fileName}" style="max-height: 300px"/></p>`
+                                    setMd(newMd)
+                                } else {
+                                    alert('이미지가 업로드 되지 못하였습니다!')
+                                }
                             }
                         }
+                    });
+                }
+
+                if (e[0].addedNodes.length > 0) {
+                    const editorTextArea = document.getElementsByClassName('w-md-editor-aree w-md-editor-input')[0] as HTMLElement
+                    // const editorPreview = document.getElementById("viewer")!.firstElementChild?.firstElementChild as HTMLElement
+                    const editorPreview = document.getElementById('md-preview') as HTMLElement
+                    editorTextArea.onscroll = (e) => {
+                        editorPreview.scrollTo(0, Math.round(editorTextArea.scrollTop * ((editorPreview.scrollHeight - editorPreview.offsetHeight) / (editorTextArea.scrollHeight - editorTextArea.offsetHeight))))
                     }
-                });
-            }
-
-            if (e[0].addedNodes.length > 0) {
-                const editorTextArea = document.getElementsByClassName('w-md-editor-aree w-md-editor-input')[0] as HTMLElement
-                // const editorPreview = document.getElementById("viewer")!.firstElementChild?.firstElementChild as HTMLElement
-                const editorPreview = document.getElementById('md-preview') as HTMLElement
-                editorTextArea.onscroll = (e) => {
-                    editorPreview.scrollTo(0, Math.round(editorTextArea.scrollTop * ((editorPreview.scrollHeight - editorPreview.offsetHeight) / (editorTextArea.scrollHeight - editorTextArea.offsetHeight))))
+                    editorPreview.onscroll = (e) => {
+                        editorTextArea.scrollTo(0, Math.round(editorPreview.scrollTop * ((editorTextArea.scrollHeight - editorTextArea.offsetHeight) / (editorPreview.scrollHeight - editorPreview.offsetHeight))))
+                    }
                 }
-                editorPreview.onscroll = (e) => {
-                    editorTextArea.scrollTo(0, Math.round(editorPreview.scrollTop * ((editorTextArea.scrollHeight - editorTextArea.offsetHeight) / (editorPreview.scrollHeight - editorPreview.offsetHeight))))
-                }
+
+
+            });
+            x.observe(document.getElementById('MDEditor_parent')!, { childList: true });
+        }
+
+
+    }, [router.isReady])
+
+    function onChangeThumbnailInput(e: ChangeEvent<HTMLInputElement>) {
+        console.log('file loaded', e)
+        if (e.target.files) {
+            API.postFile({ file: e.target.files[0], name: `thumbnail` })
+                .then((res) => {
+                    if (res.status === 201) {
+                        alert('썸네일이 업로드 되었습니다')
+                        setUrlCacheBreaker(new Date().getMilliseconds().toString())
+                    } else {
+                        alert('썸네일을 업로드하지 못하였습니다')
+                    }
+                })
+        } else {
+            alert('input에 파일이 비었습니다')
+        }
+    }
+
+    function onPasteThumbnailText(e: any) {
+        var items = (e.clipboardData || e.originalEvent.clipboardData).items;
+        for (var index in items) {
+            var item = items[index];
+            if (item.kind === 'file') {
+                let file = item.getAsFile();
+                API.postFile({ file: file, name: 'thumbnail' }).then((res) => {
+                    if (res.status === 201) {
+                        alert('썸네일이 업로드 되었습니다')
+                        setUrlCacheBreaker(new Date().getMilliseconds().toString())
+                    } else {
+                        alert('썸네일을 업로드하지 못하였습니다')
+                    }
+                })
             }
-
-
-        });
-        x.observe(document.getElementById('MDEditor_parent')!, { childList: true });
-
-    }, [])
+        }
+    }
 
 
     return (
         <div className='flex flex-col w-full mt-10'>
-
             <div className="flex-col">
                 <div className="flex mb-2 justify-between items-end">
                     <span className="w-2/3">
@@ -154,6 +191,23 @@ export default function WorkPage() {
                     </div>
                 </div>
             </div>
+            <div className="flex align-middle">
+                <img
+                    src={API.getPostFileUrl({ postId: 'temp', fileName: 'thumbnail' }) + `?${urlCacheBreaker}`} alt=""
+                    style={{ maxHeight: '100px' }}
+                />
+                <div className="flex-col w-full ml-4">
+                    <input
+                        type="file"
+                        onChange={onChangeThumbnailInput}
+                    />
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        onPaste={onPasteThumbnailText}
+                    />
+                </div>
+            </div>
             <hr className="mb-7" />
             <div className="" id="MDEditor_parent">
                 <MDEditor
@@ -167,7 +221,7 @@ export default function WorkPage() {
                     preview="edit"
                 />
             </div>
-            <div id='md-preview' style={{ padding: '50px 0', maxHeight: '400px', overflow: 'scroll' }}>
+            <div id='md-preview' style={{ padding: '50px 0', height: '600px', maxHeight: '600px', overflow: 'scroll' }}>
                 <Markdown source={md} />
             </div>
             <Button
