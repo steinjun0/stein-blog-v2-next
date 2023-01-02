1641c048d1e9b2113536dfee48ab67f1437472bb
steinjun0 <1142308@naver.com>
2023-01-02 08:58:27 +0000

    post 페이지 개선 - 코드 정리 - 해당 post 클릭시 url 연결

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,48 +1,20 @@
 import { Divider, Slide } from "@mui/material";
-import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import API from "API"
-import { marked } from "marked";
+import { marked } from "marked"
 
 
 export default function Work() {
-	const [mainCategory, setMainCategory] = useState<string>('Programming')
-	const [subCategory, setSubCategory] = useState<string>('')
-
-	const [categories, setCategories] = useState<{ mainCategories: string[], subCategories: { [key: string]: string[] } }>(
-		{
-			mainCategories: ['Programming', 'Music', 'Art', 'Life'],
-			subCategories: {
-				Programming: ['react', 'vue', 'python'],
-				Music: ['Compose', 'Record', 'Play'],
-				Art: ['Photo', 'Drawing'],
-				Life: ['log'],
-			}
-		}
-	);
-
-	const [mainCategories, setMainCategories] = useState<string[]>(['Programming', 'Music', 'Art', 'Life'])
-	const [subCategories, setSubCategories] = useState<string[]>([])
-
-
 	const [posts, setPosts] = useState<{ id: number, image: string, categories: Array<{ name: string, id: number }>, title: string, subtitle: string, body: string }[]>([])
 	useEffect(() => {
 		API.getPostList().then((res) => {
-			console.log('hi', res);
 			if (res.status === 200) {
 				setPosts(res.data)
+			} else {
+				alert('Post를 받아오지 못하였습니다')
 			}
-
 		});
-
-		// const dummyPosts = [
-		// 	{ image: '/images/DapadaSquare.png', categories: ['cat1', 'cat2'], title: '이 글의 제목은 \n"무엇이 제목인가"', subtitle: '제목과 부제목에 대한 간단한 고찰', body: ' 저번 게시글에서 캐릭터의 움직임을 구현하고, 해당 캐릭터를 따라오는 AI를 구현했다. 이제 캐릭터에 교체 가능한 무기를 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오' },
-		// 	{ image: '/images/DapadaEduSquare.png', categories: ['cat1', 'cat2'], title: '여러가지 상황\n미필적 고의도 범죄다', subtitle: '부제목은 무조건 1줄이내. 최대 27자', body: ' 저번 게시글에서 캐릭터의 움직임을 구현하고, 해당 캐릭터를 따라오는 AI를 구현했다. 이제 캐릭터에 교체 가능한 무기를 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오' },
-		// 	{ image: '/images/CareerDiveSquare.png', categories: ['cat1', 'cat2'], title: '이 글의 제목은 \n"무엇이 제목인가"', subtitle: '일이삼사오육칠팔구십일이삼사오육칠', body: ' 저번 게시글에서 캐릭터의 움직임을 구현하고, 해당 캐릭터를 따라오는 AI를 구현했다. 이제 캐릭터에 교체 가능한 무기를 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오' },
-		// 	{ image: '/images/DapadaSquare.png', categories: ['cat1', 'cat2'], title: '여러가지 상황', subtitle: '부제목의 길고 긴 길이.\n그것은 두줄이 되기에 충분했다', body: ' 저번 게시글에서 캐릭터의 움직임을 구현하고, 해당 캐릭터를 따라오는 AI를 구현했다. 이제 캐릭터에 교체 가능한 무기를 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오 장착시키고, 무기의 자동 공격을 구현해보자. 계획 1. 게임에 등장할 모델들 구현, 디자인하기 ✅ 2. 캐릭터 움직임 구현하기 ✅ 3. 캐릭터를 해당 캐릭터를 따라오' },
-		// ]
-		// setPosts(dummyPosts)
 	}, [])
 
 	return (
@@ -61,15 +33,8 @@ export default function Work() {
 				{posts.map(
 					(post, i) => (
 						<div key={i} className="w-96 mt-6 justify-center border-gray-200 border rounded-sm overflow-hidden">
-							{/* <span className='text-xs'>[{post.categories.toString().replaceAll(',', ', ')}]</span> */}
 							<Link href={`/post/${post.id}`}>
 								<div className="flex flex-col justify-center" style={{ maxHeight: 382, minHeight: 382 }}>
-									{/* <Image
-									src={API.getPostFileUrl({ postId: post.id, fileName: 'thumbnail' })}
-									alt='profile'
-									width={600}
-									height={600}
-								/> */}
 									<img
 										src={API.getPostFileUrl({ postId: post.id, fileName: 'thumbnail' })}
 										alt={`${post.id}-thumnail`}
@@ -79,7 +44,7 @@ export default function Work() {
 
 
 							<div className="flex-col p-4 pb-3 border-t border-gray-200">
-								<Link href={'/post/1'}><h1 className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700' }}>{post.title}</h1></Link>
+								<Link href={`/post/${post.id}`}><h1 className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700' }}>{post.title}</h1></Link>
 								<p className='text-sm overflow-hidden whitespace-wrap text-ellipsis mt-3 mb-3 text-gray-500'
 									style={{ WebkitLineClamp: 2, display: '-webkit-box', WebkitBoxOrient: 'vertical', fontWeight: 300 }}>
 									{marked.parse(post.body).replace(/<[^>]*>/g, '')}
@@ -92,7 +57,6 @@ export default function Work() {
 												<span>{e.name}</span>
 											</div>
 										})}
-
 									</div>
 									<span className="text-gray-400">22.11.12</span>
 								</div>
