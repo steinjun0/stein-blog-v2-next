0c69f00cba1d0d26520da7034ff92e97d26ecf7f
steinjun0 <1142308@naver.com>
2023-01-05 13:27:18 +0900

    header 추가 - favicon 변경 - meta data 추가 - open graph 추가 - page padding 화면 사이즈에 맞춰서 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,46 @@ import "styles/markdown-editor.css";
 
 import type { AppProps } from 'next/app'
 import Link from 'next/link';
+import Head from 'next/head';
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/stein-logo.svg" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>stein-blog</title>
+        <meta name="description" content="Blog for Stein. Programming, Music, Camera, Art, and Life." />
+        <meta name="author" content="Steinjun_0" />
+        <meta name="robots" content="index, follow" />
+
+        {/*  Facebook Meta Tags  */}
+        <meta property="og:url" content="https://blog.steinjun.net/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="stein-blog" />
+        <meta property="og:description" content="Blog for Stein. Programming, Music, Camera, Art, and Life." />
+        <meta property="og:image" content="/stein-logo.svg" />/
+
+        {/* Twitter Meta Tags  */}
+        <meta name="twitter:card" content="/stein-logo.svg" />
+        <meta property="twitter:domain" content="blog.steinjun.net" />
+        <meta property="twitter:url" content="https://blog.steinjun.net" />
+        <meta name="twitter:title" content="stein-blog" />
+        <meta name="twitter:description" content="Blog for Stein. Programming, Music, Camera, Art, and Life." />
+        <meta name="twitter:image" content="/stein-logo.svg" />
+      </Head>
       <nav className='flex p-4'>
         <Link href={'/'}>
-          <p className='text-3xl' style={{ fontWeight: 700 }}>Stein</p>
+          <div className='flex items-end'>
+            <p className='text-3xl' style={{ fontWeight: 700 }}>stein</p>
+            {/* <Image style={{ margin: '0 0px 4px 8px', }} src={'/stein-logo.svg'} alt={'logo'} width={24} height={24}></Image> */}
+          </div>
         </Link>
       </nav>
-      <div className='flex justify-center px-4'>
+      <div className='flex justify-center px-4 xl:px-0'>
         <Component {...pageProps} />
       </div>
     </>
