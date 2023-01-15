a8c58bc6d2c376d28b2505820e08ef43008be69a
steinjun0 <1142308@naver.com>
2023-01-15 17:47:33 +0900

    work 내부 디테일 수정 및 링크 추가

--- pages/work.tsx
+++ pages/work.tsx
@@ -5,6 +5,12 @@ import { useState } from "react";
 
 export default function Work() {
     const [category, setCategory] = useState<String>('All')
+    const imagesAndLink: { image: string, link: string }[] = [
+        { image: '/images/DapadaSquare.png', link: 'https://dapada.co.kr/app/' },
+        { image: '/images/CareerDiveSquare.png', link: 'https://careerdive.co.kr/' },
+        { image: '/images/DapadaEduSquare.png', link: 'https://dapada.co.kr/edu/' },
+        { image: '/images/DaconSquare.png', link: 'https://dacon.io/' }
+    ]
 
     return (
         <div className="flex-col  w-full mt-10 justify-start">
@@ -14,23 +20,26 @@ export default function Work() {
             </div>
             <Divider style={{ marginLeft: '186px' }} color={'black'} />
 
-            <div className="flex w-52 mt-10 justify-between [&>span]:cursor-pointer">
+            {/* <div className="flex w-52 mt-10 justify-between [&>span]:cursor-pointer">
                 {['All', 'Product', 'Project'].map(
                     (e, i) =>
                         <span key={i} className={category === e ? 'underline decoration-solid' : 'text-gray-300'}
                             onClick={() => setCategory(e)}>{e}</span>)}
-            </div>
+            </div> */}
 
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-0 mt-4">
-                {['/images/DapadaSquare.png', '/images/CareerDiveSquare.png', '/images/DapadaEduSquare.png', '/images/DaconSquare.png'].map(
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-0 mt-8">
+                {imagesAndLink.map(
                     (e, i) =>
                         <div key={i} className="relative w-full h-0"
                             style={{ paddingBottom: '100%' }}>
-                            <Image
-                                src={e}
-                                alt='work'
-                                fill
-                            />
+                            <a href={e.link}>
+                                <Image
+                                    src={e.image}
+                                    alt='work'
+                                    fill
+                                />
+                            </a>
+
                         </div>
                 )}
 
