555424032d5c9553f40857e56d1e65513264d08b
steinjun0 <1142308@naver.com>
2023-01-15 17:54:13 +0900

    post page head 추가

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,6 +1,7 @@
 import API from "API";
 import { isAxiosError } from "axios";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -50,6 +51,30 @@ export default function WorkPage() {
     return (
         <div className='flex flex-col w-full mt-10'>
             {!isLoading && <div className="flex-col">
+                <Head>
+                    <link rel="shortcut icon" href="/stein-logo.svg" />
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <title>{post!.title}</title>
+                    <meta name="description" content={post!.subtitle} />
+                    <meta name="author" content="Steinjun_0" />
+                    <meta name="robots" content="index, follow" />
+
+                    {/*  Facebook Meta Tags  */}
+                    <meta property="og:url" content={`https://blog.steinjun.net/post/${router.query.id}`} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content={post!.title} />
+                    <meta property="og:description" content={post!.subtitle} />
+                    <meta property="og:image" content="/stein-logo.svg" />/
+
+                    {/* Twitter Meta Tags  */}
+                    <meta name="twitter:card" content="/stein-logo.svg" />
+                    <meta property="twitter:domain" content="blog.steinjun.net" />
+                    <meta property="twitter:url" content={`https://blog.steinjun.net/post/${router.query.id}`} />
+                    <meta name="twitter:title" content={post!.title} />
+                    <meta name="twitter:description" content={post!.subtitle} />
+                    <meta name="twitter:image" content="/stein-logo.svg" />
+                </Head>
                 <div className="hidden md:flex mb-2 justify-between items-end">
                     <span className="text-3xl" style={{ fontWeight: 700 }}>
                         {post!.title}
