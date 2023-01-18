dcdd34fd743b6d17e7ce192d659f5c96835757df
steinjun0 <1142308@naver.com>
2023-01-18 19:24:36 +0900

    post/id meta-tag image 변경

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -80,15 +80,15 @@ export default function WorkPage(props: {
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content={props.title} />
                     <meta property="og:description" content={props.subtitle} />
-                    <meta property="og:image" content="/stein-logo.svg" />/
+                    <meta property="og:image" content={API.getPostFileUrl({ postId: props.id, fileName: 'thumbnail' })} />/
 
                     {/* Twitter Meta Tags  */}
-                    <meta name="twitter:card" content="/stein-logo.svg" />
+                    <meta name="twitter:card" content="summary_large_image" />
                     <meta property="twitter:domain" content="blog.steinjun.net" />
                     <meta property="twitter:url" content={`https://blog.steinjun.net/post/${router.query.id}`} />
                     <meta name="twitter:title" content={props.title} />
                     <meta name="twitter:description" content={props.subtitle} />
-                    <meta name="twitter:image" content="/stein-logo.svg" />
+                    <meta name="twitter:image" content={API.getPostFileUrl({ postId: props.id, fileName: 'thumbnail' })} />
                 </Head>
                 <div className="hidden md:flex mb-2 justify-between items-end">
                     <span className="text-3xl" style={{ fontWeight: 700 }}>
