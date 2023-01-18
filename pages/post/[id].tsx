e815c05e0a313c040bb7fb5ecd910f6fd7667143
steinjun0 <1142308@naver.com>
2023-01-18 19:05:41 +0900

    console.log 제거

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -19,7 +19,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     if (process.env.NODE_ENV === 'development') {
         data.body = data.body.replace('https://api.blog.steinjun.net', '//localhost:8888');
     }
-    console.log(new Date(data.created_at));
     return {
         props: {
             id: data.id,
