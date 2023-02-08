0d2c56f400c1b600cc84db3c554b19536a2e872b
steinjun0 <1142308@naver.com>
2023-02-08 21:25:35 +0900

    fix bug in /post infinity scroll

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,67 +1,80 @@
 import { Divider, FormControl, InputLabel, MenuItem, Slide } from "@mui/material";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import API from "API"
 import { marked } from "marked"
 import { useScroll } from "components/hooks/useScroll";
 import { IPost } from "components/Types";
 import Select from '@mui/material/Select'
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 
 export default function Post() {
-	const scrollHook = useScroll();
+	const router = useRouter()
+
 	const [posts, setPosts] = useState<IPost[]>([])
-	// const [postPage, setPostPage] = useState<number>(1);
-	const [postPage, setPostPage] = useState<number>(1)
-	const [isGetAllPosts, setIsGetAllPosts] = useState<boolean>(false)
-	const [isPendingApi, setIsPendingApi] = useState<boolean>(false)
+	const postNextPageRef = useRef<number>(1);
 	const [tagList, setTagList] = useState<string[]>(['All', 'Study', 'Engineering', 'Music', 'Art', 'etc'])
+	const tagRef = useRef<string>('All')
 	const [tag, setTag] = useState<string>('All')
 	const koDtf = new Intl.DateTimeFormat("ko", { dateStyle: "long" });
+	const bottomElementRef = useRef<HTMLDivElement>(null)
 
-	useEffect(() => {
-		API.getPostList({ take: 4, page: postPage, tagFilter: tag }).then((res) => {
+	async function getPosts(page: number, tag: string) {
+		await API.getPostList({ take: 6, page: page, tagFilter: tag }).then((res) => {
 			if (res.status === 200) {
-				setPosts(res.data)
+				setPosts((prev) => {
+					if (page * 6 < prev.length + res.data.length) {
+						return [...prev]
+					}
+					else {
+						if (res.data.length !== 0)
+							postNextPageRef.current = page + 1
+						return [...prev, ...res.data]
+					}
+				})
 			} else {
 				alert('Post를 받아오지 못하였습니다')
 			}
-		});
-	}, [])
+		})
+	}
 
 	useEffect(() => {
-		setPostPage(1)
-		API.getPostList({ take: 4, page: 1, tagFilter: tag }).then((res) => {
-			if (res.status === 200) {
-				setPosts(res.data)
-			} else {
-				alert('Post를 받아오지 못하였습니다')
-			}
-		});
+		tagRef.current = tag
+		getPosts(1, tag)
+		postNextPageRef.current = 1
+		return () => {
+			setPosts([])
+		}
 	}, [tag])
 
-
-
 	useEffect(() => {
-		if (!isGetAllPosts && !isPendingApi && scrollHook.scrollPercentage >= 80) {
-			setIsPendingApi(true)
-			API.getPostList({ take: 4, page: postPage + 1 }).then((res) => {
-				if (res.status === 200) {
-					if (res.data.length > 0) {
-						setPosts([...posts, ...res.data])
-						setPostPage(postPage + 1)
-						setIsPendingApi(false)
-					} else {
-						setIsGetAllPosts(true)
-					}
-
-				} else {
-					alert('Post를 받아오지 못하였습니다')
+		let observer: IntersectionObserver | null = null
+		let options: IntersectionObserverInit | null = null
+		if (router) {
+			if (router.isReady && bottomElementRef.current) {
+				options = {
+					root: document.querySelector('#scrollArea'),
+					rootMargin: '100px',
+					threshold: 0,
 				}
-			});
+
+				observer = new IntersectionObserver((e: any) => {
+					if (e[0].isIntersecting) {
+						getPosts(postNextPageRef.current, tagRef.current)
+					}
+				}, options)
+
+				observer.observe(bottomElementRef.current)
+			}
 		}
-	}, [scrollHook])
+		return () => {
+			if (observer && options) {
+				observer.disconnect()
+			}
+		}
+	}, [router, bottomElementRef])
 
 	return (
 		<div className="flex-col w-full my-10 justify-start">
@@ -148,8 +161,12 @@ export default function Post() {
 									</div>
 								</div>
 
-							</div>))}
+							</div>))
+				}
+
 			</div>
+			<div style={{ height: 300 }}></div>
+			<div id="bottom-checker" ref={bottomElementRef} />
 
 		</div >
 	);
