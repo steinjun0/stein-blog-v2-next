a751cf10da03d6f705544686d35ae90cb05e4082
steinjun0 <1142308@naver.com>
2023-02-26 18:03:57 +0900

    remove post3, post2 - post3->post

--- pages/post.tsx
+++ pages/post.tsx
@@ -8,6 +8,7 @@ import { IPost } from "components/Types";
 import Select from '@mui/material/Select'
 import Image from "next/image";
 import { useRouter } from "next/router";
+import PostCard from "components/PostCard";
 
 
 export default function Post() {
@@ -78,95 +79,86 @@ export default function Post() {
 
 	return (
 		<div className="flex-col w-full my-10 justify-start">
-			<div className="flex justify-between sm:justify-start items-end">
-				<h1 className="text-7xl mr-3 font-medium">Post</h1>
 
-				<div className="hidden sm:flex flex-wrap">
-					{
-						tagList.map(
-							(e, i) =>
-								<div key={i} className="flex items-end">
-									<div className="flex flex-col items-center">
-										{e === tag && <span className="h-4">▾</span>}
-										<div className={e === tag ? 'font-medium' : 'text-gray-400 cursor-pointer'} onClick={() => { setTag(e) }}>
-											{e}
-										</div>
-									</div>
-									<div>
-										{i < tagList.length - 1 ? <span className="text-gray-400">&nbsp;·&nbsp;</span> : ''}
-									</div>
-								</div>
+			<div className="flex flex-col justify-start">
+				<div className="flex w-full justify-between items-end">
+					<div>
+						<div>
+							<h1
+								className='no-underline hover:underline'
+								style={{ fontSize: '36px', fontWeight: '600', marginTop: '24px' }}>최근 게시글</h1>
+						</div>
+
+						<h6 style={{ fontSize: '16px', fontWeight: '400', marginTop: '4px' }}>가장 최근 올라온 게시글을 확인하세요!</h6>
+					</div>
+
+					{/* <div className="flex w-40" style={{ marginBottom: '-2px' }}>
+						<FormControl variant="standard" className="w-full" >
+							<InputLabel id="select-label">TAG</InputLabel>
+							<Select
+								onChange={(e) => { setTag(e.target.value as string) }}
+								value={tag}
+								label={'Tag'}
+								sx={{ boxShadow: 'none', '::before': { border: '0 !important' }, '::after': { border: 0 } }}
+							>
+								{tagList.map((e, i) => <MenuItem key={i} value={e}>{e}</MenuItem>)}
+							</Select>
+						</FormControl>
+					</div> */}
+				</div>
+
+
+
+
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center'>
+					{posts
+						.filter(e => process.env.NODE_ENV === 'development' || !e.categories.map(i => i.name).includes('test'))
+						.map(
+							(post, i) => (
+								<PostCard key={i} post={post} className="min-w-full" />
+								// <Link key={i} href={`/post/${post.id}`}>
+								// 	<div className="w-96 mt-6 justify-center border-gray-200 border rounded-sm overflow-hidden">
+								// 		<div className="relative" style={{ maxHeight: 382, minHeight: 382, overflow: 'hidden' }}>
+								// 			<Image
+								// 				src={API.getServerPostImageUrl({ postId: post.id, fileName: 'thumbnail' })}
+								// 				alt={`${post.id}-thumnail`}
+								// 				fill
+								// 				className="object-contain"
+								// 				priority
+								// 				sizes="(max-width: 812px) 100vw,
+								// 		(max-width: 1208px) 50vw,
+								// 		33vw"
+								// 			/>
+								// 		</div>
+
+
+								// 		<div className="flex-col p-4 pb-3 border-t border-gray-200">
+								// 			<h1 className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700', minHeight: '64px' }}>{post.title}</h1>
+								// 			<p className='text-sm overflow-hidden whitespace-wrap text-ellipsis mt-3 mb-3 text-gray-500'
+								// 				style={{ WebkitLineClamp: 2, display: '-webkit-box', WebkitBoxOrient: 'vertical', fontWeight: 300, minHeight: '40px' }}>
+								// 				{marked.parse(post.body).replace(/<[^>]*>/g, '')}
+								// 			</p>
+								// 			<hr />
+								// 			<div className="flex justify-between text-xs pt-3">
+								// 				<div className="flex flex-wrap gap-1">
+								// 					{post.categories.map((e, i) => {
+								// 						return <div key={i} className="bg-gray-700 rounded-sm text-white" style={{ padding: '2px 4px', fontWeight: 400 }}>
+								// 							<span>{e.name}</span>
+								// 						</div>
+								// 					})}
+								// 				</div>
+								// 				<span className="text-gray-400">{koDtf.format(post.created_at)}</span>
+								// 			</div>
+
+								// 		</div>
+
+								// 	</div>
+								// </Link>
+							)
 						)
 					}
+
 				</div>
-
-				<div className="flex sm:hidden w-40" style={{ marginBottom: '-2px' }}>
-					<FormControl variant="standard" className="w-full" >
-						<InputLabel id="select-label">TAG</InputLabel>
-						<Select
-							onChange={(e) => { setTag(e.target.value as string) }}
-							value={tag}
-							label={'Tag'}
-							sx={{ boxShadow: 'none', '::before': { border: '0 !important' }, '::after': { border: 0 } }}
-						>
-							{tagList.map((e, i) => <MenuItem key={i} value={e}>{e}</MenuItem>)}
-						</Select>
-					</FormControl>
-				</div>
-
-
-			</div>
-			<Slide in={true} direction={'right'}>
-				<Divider style={{ marginLeft: '158px' }} color={'black'} />
-			</Slide>
-
-			<div className="flex flex-wrap xs:justify-center gap-3 xl:justify-between mt-4">
-				{posts
-					.filter(e => process.env.NODE_ENV === 'development' || !e.categories.map(i => i.name).includes('test'))
-					.map(
-						(post, i) => (
-							<Link key={i} href={`/post/${post.id}`}>
-								<div className="w-96 mt-6 justify-center border-gray-200 border rounded-sm overflow-hidden">
-									<div className="relative" style={{ maxHeight: 382, minHeight: 382, overflow: 'hidden' }}>
-										<Image
-											src={API.getServerPostImageUrl({ postId: post.id, fileName: 'thumbnail' })}
-											alt={`${post.id}-thumnail`}
-											fill
-											className="object-contain"
-											priority
-											sizes="(max-width: 812px) 100vw,
-											(max-width: 1208px) 50vw,
-											33vw"
-										/>
-									</div>
-
-
-									<div className="flex-col p-4 pb-3 border-t border-gray-200">
-										<h1 className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700', minHeight: '64px' }}>{post.title}</h1>
-										<p className='text-sm overflow-hidden whitespace-wrap text-ellipsis mt-3 mb-3 text-gray-500'
-											style={{ WebkitLineClamp: 2, display: '-webkit-box', WebkitBoxOrient: 'vertical', fontWeight: 300, minHeight: '40px' }}>
-											{marked.parse(post.body).replace(/<[^>]*>/g, '')}
-										</p>
-										<hr />
-										<div className="flex justify-between text-xs pt-3">
-											<div className="flex flex-wrap gap-1">
-												{post.categories.map((e, i) => {
-													return <div key={i} className="bg-gray-700 rounded-sm text-white" style={{ padding: '2px 4px', fontWeight: 400 }}>
-														<span>{e.name}</span>
-													</div>
-												})}
-											</div>
-											<span className="text-gray-400">{koDtf.format(post.created_at)}</span>
-										</div>
-
-									</div>
-
-								</div>
-							</Link>
-						)
-					)
-				}
-
 			</div>
 			<div style={{ height: 300 }}></div>
 			<div id="bottom-checker" ref={bottomElementRef} />
