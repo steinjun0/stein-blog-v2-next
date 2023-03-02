a4f8ec81d3398a76c9a826d87972e4cc56863ade
steinjun0 <1142308@naver.com>
2023-03-02 17:30:35 +0900

    add subtitle to postCard Component

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -4,13 +4,14 @@ import Link from "next/link"
 import post from "pages/post"
 import Image from "next/image";
 import { IPost } from "./Types";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useRef } from "react";
 
 export default function PostCard({ post, ...props }: { post: IPost } & HTMLAttributes<HTMLDivElement>) {
     const koDtf = new Intl.DateTimeFormat("ko", { dateStyle: "long" });
+    const titleRef = useRef<HTMLHeadingElement>(null)
     return <div {...props} className="mt-6 justify-center border-gray-200 border rounded-sm overflow-hidden" style={{ minHeight: '574px' }}>
-        <Link href={`/post/${post.id}`} >
-            <div  >
+        <Link href={`/post/${post.id}`} className="flex h-full">
+            <div className="flex flex-col justify-between">
                 <div className="relative" style={{ maxHeight: 382, minHeight: 382, overflow: 'hidden' }}>
                     <Image
                         src={API.getServerPostImageUrl({ postId: post.id, fileName: 'thumbnail' })}
@@ -23,14 +24,19 @@ export default function PostCard({ post, ...props }: { post: IPost } & HTMLAttri
                     33vw"
                     />
                 </div>
-                <div className="flex-col p-4 pb-3 border-t border-gray-200">
-                    <h1 className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700', minHeight: '64px' }}>{post.title}</h1>
-                    <p className='text-sm overflow-hidden whitespace-wrap text-ellipsis mt-3 mb-3 text-gray-500'
-                        style={{ WebkitLineClamp: 2, display: '-webkit-box', WebkitBoxOrient: 'vertical', fontWeight: 300, minHeight: '40px' }}>
+                <div className="flex flex-col justify-between p-4 pb-3 border-t border-gray-200 h-full">
+                    <div className="flex flex-col">
+                        <h1 ref={titleRef} className='text-2xl whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '700' }}>{post.title}</h1>
+                        <h4 className='whitespace-pre-wrap [&:hover]:underline' style={{ fontWeight: '500', minHeight: '32px', fontSize: '16px' }}>{post.subtitle}</h4>
+                    </div>
+                    <p className='text-sm overflow-hidden whitespace-wrap text-ellipsis mt-3 text-gray-500'
+                        style={{ WebkitLineClamp: titleRef.current?.clientHeight === 32 ? 3 : 2, display: '-webkit-box', WebkitBoxOrient: 'vertical', fontWeight: 300, minHeight: '40px' }}>
                         {marked.parse(post.body).replace(/<[^>]*>/g, '')}
                     </p>
+                </div>
+                <div>
                     <hr />
-                    <div className="flex justify-between text-xs pt-3">
+                    <div className="flex justify-between text-xs p-4 mt-auto">
                         <div className="flex flex-wrap gap-1">
                             {post.categories.map((e, i) => {
                                 return <div key={i} className="bg-gray-700 rounded-sm text-white" style={{ padding: '2px 4px', fontWeight: 400 }}>
@@ -41,6 +47,7 @@ export default function PostCard({ post, ...props }: { post: IPost } & HTMLAttri
                         <span className="text-gray-400">{koDtf.format(post.created_at)}</span>
                     </div>
                 </div>
+
             </div>
         </Link>
     </div>
