684555555ceded316c69a5172cf2a322e43b23d9
steinjun0 <1142308@naver.com>
2023-04-16 16:07:45 +0900

    comment out oldAPI files

--- API.ts
+++ API.ts
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = process.platform === 'linux' ? process.env.NEXT_PUBLIC_SERVER_API_URL : process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.platform === 'linux' ? process.env.NEXT_PUBLIC_SERVER_API_URL : process.env.NEXT_PUBLIC_API_URL;
 // let accessToken = localStorage.getItem('AccessToken')
-let accessToken = null
-let tokenHeader: { Authorization: string } | null = null
+let accessToken = null;
+let tokenHeader: { Authorization: string; } | null = null;
 
 
 if (accessToken !== null) {
-    tokenHeader = { Authorization: `${accessToken}` }
+    tokenHeader = { Authorization: `${accessToken}` };
 } else {
-    tokenHeader = null
+    tokenHeader = null;
 }
 
 const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?/;
@@ -19,22 +19,22 @@ function isIsoDateString(value: any): boolean {
 }
 
 export function handleDates(body: string) {
-    const jsonBody: { string: any } = JSON.parse(body)
+    const jsonBody: { string: any; } = JSON.parse(body);
 
-    function recursiveChanger(json: { string: any }) {
+    function recursiveChanger(json: { string: any; }) {
         if (json !== null) {
             for (const key of Object.keys(json)) {
-                const value = json[key as keyof { string: any }];
+                const value = json[key as keyof { string: any; }];
                 if (typeof value === 'object')
-                    recursiveChanger(value)
+                    recursiveChanger(value);
                 else if (isIsoDateString(value)) {
-                    json[key as keyof { string: any }] = new Date(value);
+                    json[key as keyof { string: any; }] = new Date(value);
                 }
             }
         }
     }
-    recursiveChanger(jsonBody)
-    return jsonBody
+    recursiveChanger(jsonBody);
+    return jsonBody;
 }
 
 
@@ -43,28 +43,28 @@ export default {
 
     refreshUserData() {
         // let accessToken = localStorage.getItem('AccessToken')
-        let accessToken = null
+        let accessToken = null;
 
         if (accessToken !== null) {
-            tokenHeader = { Authorization: `${accessToken}` }
+            tokenHeader = { Authorization: `${accessToken}` };
         } else {
-            tokenHeader = null
+            tokenHeader = null;
         }
     },
     async getAxios(url: string) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.get(url, { transformResponse: handleDates, headers: tokenHeader })
-            : await axios.get(url, { transformResponse: handleDates })
-        return res
+            : await axios.get(url, { transformResponse: handleDates });
+        return res;
     },
     async getAxiosZip(url: string) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.get(url, { headers: { Authorization: tokenHeader.Authorization, accept: 'application/x-zip-compressed' } })
-            : await axios.get(url, { headers: { accept: 'application/x-zip-compressed' }, transformResponse: handleDates })
-        return res
+            : await axios.get(url, { headers: { accept: 'application/x-zip-compressed' }, transformResponse: handleDates });
+        return res;
     },
     async getAxiosWithParams(url: string, param: object) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.get(url, {
             transformResponse: handleDates,
             headers: tokenHeader,
@@ -72,121 +72,120 @@ export default {
         }) : await axios.get(url, {
             transformResponse: handleDates,
             params: param,
-        })
-        return res
+        });
+        return res;
     },
     async postAxios(url: string, data: object) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.post(url, data, { transformResponse: handleDates, headers: tokenHeader })
-            : await axios.post(url, data, { transformResponse: handleDates })
-        return res
+            : await axios.post(url, data, { transformResponse: handleDates });
+        return res;
     },
     async postAxiosFormData(url: string, data: object) {
         // You need to make data as a FormData
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.post(url, data, { headers: { Authorization: tokenHeader.Authorization, 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
-            : await axios.post(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
-        return res
+            : await axios.post(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates });
+        return res;
     },
     async patchAxios(url: string, data: object) {
-        this.refreshUserData()
-        const res = tokenHeader ? await axios.patch(url, data, { headers: tokenHeader, transformResponse: handleDates }) : await axios.patch(url, data, { transformResponse: handleDates })
-        return res
+        this.refreshUserData();
+        const res = tokenHeader ? await axios.patch(url, data, { headers: tokenHeader, transformResponse: handleDates }) : await axios.patch(url, data, { transformResponse: handleDates });
+        return res;
     },
     async patchAxiosFormData(url: string, data: object) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.patch(url, data, { headers: { Authorization: tokenHeader.Authorization, 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
-            : await axios.patch(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates })
-        return res
+            : await axios.patch(url, data, { headers: { 'Content-Type': 'multipart/form-data' }, transformResponse: handleDates });
+        return res;
     },
 
     async putAxios(url: string) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.put(url, { headers: tokenHeader, transformResponse: handleDates })
-            : await axios.put(url, { transformResponse: handleDates })
-        return res
+            : await axios.put(url, { transformResponse: handleDates });
+        return res;
     },
 
     async deleteAxios(url: string) {
-        this.refreshUserData()
+        this.refreshUserData();
         const res = tokenHeader ? await axios.delete(url, { headers: tokenHeader, transformResponse: handleDates })
-            : await axios.delete(url, { transformResponse: handleDates })
-        return res
+            : await axios.delete(url, { transformResponse: handleDates });
+        return res;
     },
 
 
     // static link
-    getOrigin() {
-        return API_URL
-    },
+    // getOrigin() {
+    //     return API_URL;
+    // },
 
-    getServerPostImageUrl({ postId, fileName }: { postId: number | 'temp', fileName: string }) {
-        return `${process.env.NEXT_PUBLIC_SERVER_API_URL}/file/post/${postId}/${fileName}`
-    },
+    // getServerPostImageUrl({ postId, fileName }: { postId: number | 'temp', fileName: string; }) {
+    //     return `${process.env.NEXT_PUBLIC_SERVER_API_URL}/file/post/${postId}/${fileName}`;
+    // },
 
-    getPostFileUrl({ postId, fileName }: { postId: number | 'temp', fileName: string }) {
-        return `${API_URL}/file/post/${postId}/${fileName}`
-    },
+    // getPostFileUrl({ postId, fileName }: { postId: number | 'temp', fileName: string; }) {
+    //     return `${API_URL}/file/post/${postId}/${fileName}`;
+    // },
 
     // api
 
-    async getPostList(option?: { page?: number, take?: number, tagFilter?: string }) {
-        if (option) {
-            const postListRes = await this.getAxios(`${API_URL}/post?${option.tagFilter ? `tagFilter=${option.tagFilter}&` : ''}${option.page ? `page=${option.page}&` : ''}${option.take ? `take=${option.take}&` : ''}`)
-            return postListRes
-        } else {
-            const postListRes = await this.getAxios(`${API_URL}/post`)
-            return postListRes
-        }
-    },
+    // async getPostList(option?: { page?: number, take?: number, tagFilter?: string; }) {
+    //     if (option) {
+    //         const postListRes = await this.getAxios(`${API_URL}/post?${option.tagFilter ? `tagFilter=${option.tagFilter}&` : ''}${option.page ? `page=${option.page}&` : ''}${option.take ? `take=${option.take}&` : ''}`);
+    //         return postListRes;
+    //     } else {
+    //         const postListRes = await this.getAxios(`${API_URL}/post`);
+    //         return postListRes;
+    //     }
+    // },
 
-    async getPost({ id }: { id: number }) {
-        const postRes = await this.getAxios(`${API_URL}/post/${id}`)
-        return postRes
-    },
+    // async getPost({ id }: { id: number; }) {
+    //     const postRes = await this.getAxios(`${API_URL}/post/${id}`);
+    //     return postRes;
+    // },
 
-    async getPostsByIds({ ids }: { ids: number[] }) {
-        const postRes = await this.getAxios(`${API_URL}/post?${ids.map(id => `ids=${id}&`).join('')}`)
-        return postRes
-    },
+    // async getPostsByIds({ ids }: { ids: number[]; }) {
+    //     const postRes = await this.getAxios(`${API_URL}/post?${ids.map(id => `ids=${id}&`).join('')}`);
+    //     return postRes;
+    // },
 
-    async getCategories() {
-        const categoryRes = await this.getAxios(`${API_URL}/post/category`)
-        return categoryRes
-    },
+    // async getCategories() {
+    //     const categoryRes = await this.getAxios(`${API_URL}/post/category`);
+    //     return categoryRes;
+    // },
 
+    // async getIsAdmin({ accessToken }: { accessToken: string }) {
+    //     const isAdminRes = await this.getAxios(`${API_URL}/etc/auth/admin?access-token=${accessToken}`)
+    //     return isAdminRes
+    // },
 
-    async getIsAdmin({ accessToken }: { accessToken: string }) {
-        const isAdminRes = await this.getAxios(`${API_URL}/etc/auth/admin?access-token=${accessToken}`)
-        return isAdminRes
-    },
+    // async getBaekjoonData() {
+    //     const baekjoonRes = await this.getAxios(`${API_URL}/etc/baekjoon/solved-problems`)
+    //     return baekjoonRes
+    // },
 
-    async getBaekjoonData() {
-        const baekjoonRes = await this.getAxios(`${API_URL}/etc/baekjoon/solved-problems`)
-        return baekjoonRes
-    },
-
-    async getSolvedacData() {
-        const solvedacRes = await this.getAxios(`${API_URL}/etc/solvedac/data`)
-        return solvedacRes
-    },
+    // async getSolvedacData() {
+    //     const solvedacRes = await this.getAxios(`${API_URL}/etc/solvedac/data`)
+    //     return solvedacRes
+    // },
 
     // async getConsultSchedule(year, month, mentorId) {
     //     const scheduleRes = await this.getAxiosWithParams(`${ API_URL }/consult/schedule`, { 'Year': year, 'Month': month, 'MentorID': +mentorId })
     //     return scheduleRes
     // },
 
-    async postFile({ file, name }: { file: Blob, name?: string }) {
-        const data = new FormData();
-        data.append('file', file, name)
-        const res = await this.postAxiosFormData(`${API_URL}/file/post`, data)
-        return res
-    },
+    // async postFile({ file, name }: { file: Blob, name?: string; }) {
+    //     const data = new FormData();
+    //     data.append('file', file, name);
+    //     const res = await this.postAxiosFormData(`${API_URL}/file/post`, data);
+    //     return res;
+    // },
 
-    async postPost(data: { title: string, subtitle: string, body: string, categories?: Array<string>, files?: Array<string> }) {
-        const scheduleRes = await this.postAxios(`${API_URL}/post`, data)
-        return scheduleRes
-    },
+    // async postPost(data: { title: string, subtitle: string, body: string, categories?: Array<string>, files?: Array<string>; }) {
+    //     const scheduleRes = await this.postAxios(`${API_URL}/post`, data);
+    //     return scheduleRes;
+    // },
 
     // async patchAccount(userData) {
     //     const userRes = await this.patchAxios(`${ API_URL }/account/update`, userData)
@@ -194,15 +193,15 @@ export default {
     // },
 
 
-    async patchPost(postId: number, data: { title: string, subtitle: string, body: string, categories?: Array<string>, files?: Array<string> }) {
-        const postRes = await this.patchAxios(`${API_URL}/post/${postId}`, data)
-        return postRes
-    },
+    // async patchPost(postId: number, data: { title: string, subtitle: string, body: string, categories?: Array<string>, files?: Array<string>; }) {
+    //     const postRes = await this.patchAxios(`${API_URL}/post/${postId}`, data);
+    //     return postRes;
+    // },
 
-    async deletePost(postId: number) {
-        const postRes = await this.deleteAxios(`${API_URL}/post/${postId}`)
-        return postRes
-    },
+    // async deletePost(postId: number) {
+    //     const postRes = await this.deleteAxios(`${API_URL}/post/${postId}`);
+    //     return postRes;
+    // },
 
 
-}
+};
