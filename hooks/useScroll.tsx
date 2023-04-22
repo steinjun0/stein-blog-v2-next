015e4962f337fd52b2d09dd2a7e76538a4e4515f
steinjun0 <1142308@naver.com>
2023-04-22 17:31:03 +0900

    remove unnecessary logic in useScroll

--- hooks/useScroll.tsx
+++ hooks/useScroll.tsx
@@ -2,27 +2,22 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 export function useScroll() {
-    const router = useRouter()
-
     const [lastScrollTop, setLastScrollTop] = useState<number>(0);
-    const [bodyOffset, setBodyOffset] = useState<DOMRect>();
     const [scrollY, setScrollY] = useState<number>(1);
     const [scrollX, setScrollX] = useState<number>(1);
     const [scrollDirection, setScrollDirection] = useState<'down' | 'up'>();
     const [scrollPercentage, setScrollPercentage] = useState<number>(0);
 
     const listener = (e: Event) => {
-        if (router.isReady) {
-            let h = document.documentElement, b = document.body
-            let percent = (h.scrollTop || b.scrollTop) / ((h.scrollHeight || b.scrollHeight) - h.clientHeight) * 100;
-            setBodyOffset(document.body.getBoundingClientRect());
-            if (bodyOffset) {
-                setScrollY(-bodyOffset.top);
-                setScrollX(bodyOffset.left);
-                setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
-                setLastScrollTop(-bodyOffset.top);
-                setScrollPercentage(percent)
-            }
+        let h = document.documentElement, b = document.body;
+        let percent = (h.scrollTop || b.scrollTop) / ((h.scrollHeight || b.scrollHeight) - h.clientHeight) * 100;
+        const bodyOffset = document.body.getBoundingClientRect();
+        if (bodyOffset) {
+            setScrollY(-bodyOffset.top);
+            setScrollX(bodyOffset.left);
+            setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
+            setLastScrollTop(-bodyOffset.top);
+            setScrollPercentage(percent);
         }
     };
 
@@ -32,7 +27,6 @@ export function useScroll() {
             window.removeEventListener("scroll", listener);
         };
     });
-
     return {
         scrollY,
         scrollX,
