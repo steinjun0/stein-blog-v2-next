58879492dac7582fa9c2fce768c3b18bad0c78f9
steinjun0 <1142308@naver.com>
2023-04-22 17:36:49 +0900

    remove old API import code

--- components/ListPushInput.tsx
+++ components/ListPushInput.tsx
@@ -1,28 +1,25 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import API from "API"
-import { useEffect } from "react";
 import { Button, TextField } from "@mui/material";
 
-function ListPushInput({ categoryList, setCategoryList }: { categoryList: string[], setCategoryList: Dispatch<SetStateAction<string[]>> }) {
+function ListPushInput({ categoryList, setCategoryList }: { categoryList: string[], setCategoryList: Dispatch<SetStateAction<string[]>>; }) {
     const [categoryText, setCategoryText] = useState('');
 
     const addNewCategory = () => {
-        console.log('categoryText', categoryText)
-        setCategoryList([...categoryList, categoryText])
-        setCategoryText('')
-    }
+        console.log('categoryText', categoryText);
+        setCategoryList([...categoryList, categoryText]);
+        setCategoryText('');
+    };
 
     const deleteCategory = (deletedCategory: string) => {
         const newCategoryList = categoryList.filter((e) => {
             if (e !== deletedCategory) {
-                return true
+                return true;
             } else {
-                return false
+                return false;
             }
-        })
-        setCategoryList(newCategoryList)
-    }
+        });
+        setCategoryList(newCategoryList);
+    };
 
 
     return <div>
@@ -30,12 +27,12 @@ function ListPushInput({ categoryList, setCategoryList }: { categoryList: string
             <TextField
                 onKeyPress={(e) => {
                     if (e.key === 'Enter' && categoryText !== '' && !categoryList.includes(categoryText)) {
-                        addNewCategory()
+                        addNewCategory();
                     }
                 }}
                 value={categoryText}
                 onChange={(e) => {
-                    setCategoryText(e.target.value)
+                    setCategoryText(e.target.value);
                 }}
                 fullWidth={true}
                 placeholder={'입력 후 Enter'}
@@ -43,7 +40,7 @@ function ListPushInput({ categoryList, setCategoryList }: { categoryList: string
             <Button
                 onClick={() => {
                     if (categoryText !== '' && !categoryList.includes(categoryText)) {
-                        addNewCategory()
+                        addNewCategory();
                     }
                 }}>
                 확인
