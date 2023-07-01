17bf87d273ba03ca21f8953ba4cf0f51edb55c70
steinjun0 <1142308@naver.com>
2023-07-01 18:21:09 +0900

    commentout kakaocdn script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 declare global {
   interface Window {
     Kakao: any,
-    dataLayer: any
+    dataLayer: any;
   }
 }
 export default function Document() {
@@ -12,13 +12,13 @@ export default function Document() {
     <Html>
       <Head />
       <body>
-        <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
+        {/* <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
           crossOrigin="anonymous"
           strategy='beforeInteractive'
-        ></Script>
+        ></Script> */}
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
